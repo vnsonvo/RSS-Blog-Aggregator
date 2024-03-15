53eb53e7df1f7fd59579b4ac66868c0b2705ddcf
Son Vo <vnsonvo@gmail.com>
2024-03-15 11:00:22 +0700

    update middleware cor

--- header.go
+++ header.go
@@ -1,15 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		if req.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
+		origin := req.Header.Get("Origin")
+		if origin != "" {
+			if strings.HasPrefix(origin, "http://") || strings.HasPrefix(origin, "https://") {
+				w.Header().Set("Access-Control-Allow-Origin", origin)
+			}
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "*")
+			w.Header().Set("Access-Control-Expose-Headers", "Link")
+			w.Header().Set("Access-Control-Max-Age", "300") // 5 minutes
+
+			if req.Method == "OPTIONS" {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
 		}
 		next.ServeHTTP(w, req)
 	})
