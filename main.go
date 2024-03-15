203eb673da1194dee69693de399d73d6ec11e7a4
Son Vo <vnsonvo@gmail.com>
2024-03-15 10:25:37 +0700

    add gitignore file and use godotenv to load env variable

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Init Project")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	fmt.Println(port)
 }
