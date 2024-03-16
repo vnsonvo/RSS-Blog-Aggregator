4fccb7667873a97c32b387d2ae82095286e5cdbe
Son Vo <vnsonvo@gmail.com>
2024-03-16 14:51:50 +0700

    add endpoint to create user and use shared struct to share config

---
+++ handler_user.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/vnsonvo/RSS-Blog-Aggregator/internal/database"
+)
+
+func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, req *http.Request) {
+	type parameters struct {
+		Name string `json:"name"`
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, fmt.Sprintf("Couldn't decode parameters: %s", err))
+		return
+	}
+
+	newUser, err := apiCfg.DB.CreateUser(req.Context(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+	})
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, fmt.Sprintf("Couldn't create user: %s", err))
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, databaseUserToUser(newUser))
+}
--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/vnsonvo/RSS-Blog-Aggregator/internal/database"
+
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env file", err)
 	}
 
 	port := os.Getenv("PORT")
@@ -19,10 +27,27 @@ func main() {
 		log.Fatal("PORT not found in the environment")
 	}
 
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is not found in the environment")
+	}
+
+	connection, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect to database", err)
+	}
+
+	dbQueries := database.New(connection)
+
+	apiCfg := apiConfig{
+		DB: dbQueries,
+	}
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /v1/readiness", handlerReadiness)
 	mux.HandleFunc("GET /v1/err", handlerErr)
+	mux.HandleFunc("POST /v1/users", apiCfg.handlerCreateUser)
 
 	corsMux := middlewareCors(mux)
 
